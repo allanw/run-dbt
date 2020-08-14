52e6418a0745f744c3f5fbde72e6c149bfc89f5e
Al Whatmough <al.whatmough@gmail.com>
2020-08-14 17:52:17 +1000

    change time_asleep column to be a float value (in hours)

--- models/base/oura/sleeps.sql
+++ models/base/oura/sleeps.sql
@@ -4,7 +4,7 @@ with sleeps as (
         bedtime_start,
         bedtime_end,
         bedtime_end - bedtime_start as time_in_bed,
-        to_char((duration - awake) * '1 second'::interval, 'HH24:MI:SS') as time_asleep
+        round((duration - awake) / 60.0 / 60, 2) as time_asleep
     from tap_oura.sleeps
 )
 
